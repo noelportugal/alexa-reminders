f091e98aaf5a259fa10757a6ea8642889933cafa
Noel Portugal <noelportugal@gmail.com>
2017-12-09 08:06:56 -1000

    remove redundant example

--- example.js
+++
@@ -1,7 +0,0 @@
-var reminders = require('./alexa-reminders')
-
-reminders.login('Device Name', 'username', 'password', function(error, response, config){
-  reminders.setReminder('Ask Alexa team for a proper Reminders API', null, config, function(error, response){
-    console.log(response)
-  })
-})
